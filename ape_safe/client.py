090611666a47b661889477da6c03bb5411c24d79
doggie <3859395+fubuloubu@users.noreply.github.com>
2023-06-28 18:04:26 -0400

    refactor: Use ABC for SafeClient to support Mock

--- ape_safe/client.py
+++ ape_safe/client.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from datetime import datetime
 from enum import Enum
 from functools import reduce
@@ -123,7 +124,74 @@ class ExecutedTxData(UnexecutedTxData):
 SafeApiTxData = Union[ExecutedTxData, UnexecutedTxData]
 
 
-class SafeClient:
+class BaseSafeClient(ABC):
+    @property
+    @abstractmethod
+    def safe_details(self) -> SafeDetails:
+        ...
+
+    @abstractmethod
+    def get_next_nonce(self) -> int:
+        ...
+
+    @abstractmethod
+    def _all_transactions(self) -> Iterator[SafeApiTxData]:
+        ...
+
+    def get_transactions(
+        self,
+        confirmed: Optional[bool] = None,
+        starting_nonce: int = 0,
+        filter_by_ids: Optional[Set[SafeTxID]] = None,
+        filter_by_missing_signers: Optional[Set[AddressType]] = None,
+    ) -> Iterator[SafeApiTxData]:
+        """
+        confirmed: Confirmed if True, not confirmed if False, both if None
+        """
+        next_nonce = self.get_next_nonce()
+
+        for txn in self._all_transactions():
+            if txn.nonce < starting_nonce:
+                break  # NOTE: order is largest nonce to smallest, so safe to break here
+
+            isConfirmed = len(txn.confirmations) >= txn.confirmationsRequired
+
+            if confirmed is not None:
+                if not confirmed and isinstance(txn, ExecutedTxData):
+                    break  # NOTE: Break at the first executed transaction
+
+                elif confirmed and not isConfirmed:
+                    continue  # NOTE: Skip not confirmed transactions
+
+            if txn.nonce < next_nonce and isinstance(txn, UnexecutedTxData):
+                continue  # NOTE: Skip orphaned transactions
+
+            if filter_by_ids and txn.safeTxHash not in filter_by_ids:
+                continue  # NOTE: Skip transactions not in the filter
+
+            if filter_by_missing_signers and filter_by_missing_signers.issubset(
+                set(conf.owner for conf in txn.confirmations)
+            ):
+                # NOTE: Skip if all signers from `filter_by_missing_signers`
+                #       are in `txn.confirmations`
+                continue
+
+            yield txn
+
+    @abstractmethod
+    def get_confirmations(self, safe_tx_hash: SafeTxID) -> Iterator[SafeTxConfirmation]:
+        ...
+
+    @abstractmethod
+    def post_transaction(self, safe_tx: SafeTx, sigs: Optional[List[MessageSignature]] = None):
+        ...
+
+    @abstractmethod
+    def post_signature(self, safe_tx_hash: SafeTxID, signature: MessageSignature):
+        ...
+
+
+class SafeClient(BaseSafeClient):
     def __init__(
         self,
         address: AddressType,
@@ -158,7 +226,7 @@ class SafeClient:
     def get_next_nonce(self) -> int:
         return self.safe_details.nonce
 
-    def _all_transactions(self) -> Iterator[dict]:
+    def _all_transactions(self) -> Iterator[SafeApiTxData]:
         """
         confirmed: Confirmed if True, not confirmed if False, both if None
         """
@@ -170,49 +238,15 @@ class SafeClient:
                 raise ClientResponseError(url, response)
 
             data = response.json()
-            yield from data["results"]
+
+            for txn in data["results"]:
+                if "isExecuted" in txn and txn["isExecuted"]:
+                    yield ExecutedTxData.parse_obj(txn)
+
+                else:
+                    yield UnexecutedTxData.parse_obj(txn)
+
             url = data["next"]
-
-    def get_transactions(
-        self,
-        confirmed: Optional[bool] = None,
-        starting_nonce: int = 0,
-        filter_by_ids: Optional[Set[SafeTxID]] = None,
-        filter_by_missing_signers: Optional[Set[AddressType]] = None,
-    ) -> Iterator[SafeApiTxData]:
-        """
-        confirmed: Confirmed if True, not confirmed if False, both if None
-        """
-        next_nonce = self.get_next_nonce()
-
-        for txn in self._all_transactions():
-            if txn["nonce"] < starting_nonce:
-                break  # NOTE: order is largest nonce to smallest, so safe to break here
-
-            isConfirmed = len(txn["confirmations"]) >= txn["confirmationsRequired"]
-            isExecuted = "isExecuted" in txn and txn["isExecuted"]
-
-            if confirmed is not None:
-                if not confirmed and isExecuted:
-                    break  # NOTE: Break at the first executed transaction
-
-                elif confirmed and not isConfirmed:
-                    continue  # NOTE: Skip not confirmed transactions
-
-            if txn["nonce"] < next_nonce and not isExecuted:
-                continue  # NOTE: Skip orphaned transactions
-
-            if filter_by_ids and txn["safeTxHash"] not in filter_by_ids:
-                continue  # NOTE: Skip transactions not in the filter
-
-            if filter_by_missing_signers and filter_by_missing_signers.issubset(
-                set(conf["owner"] for conf in txn["confirmations"])
-            ):
-                # NOTE: Skip if all signers from `filter_by_missing_signers`
-                #       are in `txn.confirmations`
-                continue
-
-            yield ExecutedTxData.parse_obj(txn) if isExecuted else UnexecutedTxData.parse_obj(txn)
 
     def get_confirmations(self, safe_tx_hash: SafeTxID) -> Iterator[SafeTxConfirmation]:
         url = (
