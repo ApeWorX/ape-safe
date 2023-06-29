14b3318b8d4dbe2b2f7007be43eac981d459ae17
doggie <3859395+fubuloubu@users.noreply.github.com>
2023-06-29 18:51:27 -0400

    test: show skipping submit can be executed from mock client

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,4 +1,5 @@
 import pytest
+from ape.exceptions import SignatureError
 
 
 def test_init(safe, OWNERS, THRESHOLD, safe_contract):
@@ -8,9 +9,11 @@ def test_init(safe, OWNERS, THRESHOLD, safe_contract):
     assert safe.next_nonce == 0
 
 
-@pytest.mark.parametrize("mode", ["impersonate", "sign"])
+@pytest.mark.parametrize("mode", ["impersonate", "api", "sign"])
 def test_swap_owner(safe, accounts, OWNERS, mode):
     impersonate = mode == "impersonate"
+    submit = mode != "api"
+
     old_owner = safe.signers[0]
     new_owner = accounts[len(OWNERS)]  # replace owner 1 with account N + 1
     assert new_owner.address not in safe.signers
@@ -18,14 +21,30 @@ def test_swap_owner(safe, accounts, OWNERS, mode):
 
     prev_owner = safe.compute_prev_signer(old_owner)
 
-    # TODO: Remove `gas_limit` by allowing forking to compute gas limit
-    receipt = safe.contract.swapOwner(
+    exec_transaction = lambda: safe.contract.swapOwner(  # noqa: E731
         prev_owner,
         old_owner,
         new_owner,
         sender=safe,
         impersonate=impersonate,
+        submit=submit,
     )
+
+    if submit:
+        receipt = exec_transaction()
+
+    else:
+        # Attempting to execute should emit a `SignatureError` and push `safe_tx` to mock client
+        assert len(list(safe.client.get_transactions(confirmed=False))) == 0
+        with pytest.raises(SignatureError):
+            exec_transaction()
+
+        assert len(list(safe.client.get_transactions(confirmed=False))) == 1
+
+        # `safe_tx` is in mock client, extreact it and execute it successfully this time
+        safe_tx_data = next(safe.client.get_transactions(confirmed=False))
+        safe_tx = safe.create_safe_tx(**safe_tx_data.dict())
+        receipt = safe.submit_safe_tx(safe_tx)
 
     assert receipt.events == [
         safe.contract.RemovedOwner(owner=old_owner),
@@ -37,19 +56,37 @@ def test_swap_owner(safe, accounts, OWNERS, mode):
     assert new_owner.address in safe.signers
 
 
-@pytest.mark.parametrize("mode", ["impersonate", "sign"])
+@pytest.mark.parametrize("mode", ["impersonate", "api", "sign"])
 def test_add_owner(safe, accounts, OWNERS, mode):
     impersonate = mode == "impersonate"
+    submit = mode != "api"
+
     new_owner = accounts[len(OWNERS)]  # replace owner 1 with account N + 1
     assert new_owner.address not in safe.signers
 
-    # TODO: Remove `gas_limit` by allowing forking to compute gas limit
-    receipt = safe.contract.addOwnerWithThreshold(
+    exec_transaction = lambda: safe.contract.addOwnerWithThreshold(  # noqa: E731
         new_owner,
         safe.confirmations_required,
         sender=safe,
         impersonate=impersonate,
+        submit=submit,
     )
+
+    if submit:
+        receipt = exec_transaction()
+
+    else:
+        # Attempting to execute should emit a `SignatureError` and push `safe_tx` to mock client
+        assert len(list(safe.client.get_transactions(confirmed=False))) == 0
+        with pytest.raises(SignatureError):
+            exec_transaction()
+
+        assert len(list(safe.client.get_transactions(confirmed=False))) == 1
+
+        # `safe_tx` is in mock client, extreact it and execute it successfully this time
+        safe_tx_data = next(safe.client.get_transactions(confirmed=False))
+        safe_tx = safe.create_safe_tx(**safe_tx_data.dict())
+        receipt = safe.submit_safe_tx(safe_tx)
 
     assert receipt.events == [
         safe.contract.AddedOwner(owner=new_owner),
@@ -59,9 +96,10 @@ def test_add_owner(safe, accounts, OWNERS, mode):
     assert new_owner.address in safe.signers
 
 
-@pytest.mark.parametrize("mode", ["impersonate", "sign"])
+@pytest.mark.parametrize("mode", ["impersonate", "api", "sign"])
 def test_remove_owner(safe, OWNERS, mode):
     impersonate = mode == "impersonate"
+    submit = mode != "api"
     if len(OWNERS) == 1:
         pytest.skip("Can't remove the only owner")
 
@@ -70,14 +108,31 @@ def test_remove_owner(safe, OWNERS, mode):
     threshold_changed = new_threshold != safe.confirmations_required
 
     prev_owner = safe.compute_prev_signer(old_owner)
-    receipt = safe.contract.removeOwner(
+    exec_transaction = lambda: safe.contract.removeOwner(  # noqa: E731
         prev_owner,
         old_owner,
         # Can't set the threshold to zero or more than the number of owners after removal
         new_threshold,
         sender=safe,
         impersonate=impersonate,
+        submit=submit,
     )
+
+    if submit:
+        receipt = exec_transaction()
+
+    else:
+        # Attempting to execute should emit a `SignatureError` and push `safe_tx` to mock client
+        assert len(list(safe.client.get_transactions(confirmed=False))) == 0
+        with pytest.raises(SignatureError):
+            exec_transaction()
+
+        assert len(list(safe.client.get_transactions(confirmed=False))) == 1
+
+        # `safe_tx` is in mock client, extreact it and execute it successfully this time
+        safe_tx_data = next(safe.client.get_transactions(confirmed=False))
+        safe_tx = safe.create_safe_tx(**safe_tx_data.dict())
+        receipt = safe.submit_safe_tx(safe_tx)
 
     expected_events = [
         safe.contract.RemovedOwner(owner=old_owner),
