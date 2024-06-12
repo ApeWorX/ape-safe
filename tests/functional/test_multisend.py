b4e7943a1694bc0ff4e88e571afb3681b0eff35d
banteg <4562643+banteg@users.noreply.github.com>
2024-06-13 03:25:45 +0400

    feat: multisend contract detection (#58)
    * feat: detect multisend contract instead of hardcoding
    
    * feat: disallow delegatecall in multisend calls
    
    * lint: happy linter
    
    * refactor: use manifest for multisend abi and code
    
    * refactor: rename data to manifests
    
    * lint: mypy
    
    * lint: shut up robot
    
    * test: remove test
    
    multisend call only doesn't enforce being delegatecalled
    
    * lint: happy bot

--- tests/functional/test_multisend.py
+++ tests/functional/test_multisend.py
@@ -1,8 +1,3 @@
-import pytest
-
-from ape_safe.exceptions import SafeLogicError
-
-
 def test_asset(vault, token):
     assert vault.asset() == token
 
@@ -13,14 +8,6 @@ def test_default_operation(safe, token, vault, multisend):
     multisend.add(vault.transfer, safe, amount)
     receipt = multisend(sender=safe)
     assert receipt.txn_hash
-
-
-def test_no_operation(safe, token, vault, multisend):
-    amount = token.balanceOf(safe)
-    multisend.add(token.approve, vault, 123)
-    multisend.add(vault.transfer, safe, amount)
-    with pytest.raises(SafeLogicError, match="Safe transaction failed"):
-        multisend(sender=safe, operation=0)
 
 
 def test_decode_multisend(multisend):
