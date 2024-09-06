2a1be4420c3e7746e6a6db70cafed30627a43bfe
Dalena <mimidalena@gmail.com>
2024-09-06 11:28:19 -0500

    chore: dependencies

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ extras_require = {
         "ape-solidity",  # Needed for compiling the Safe contracts
     ],
     "lint": [
-        "black>=24.4.2,<25",  # Auto-formatter and linter
-        "mypy>=1.10.0,<2",  # Static type analyzer
+        "black>=24.8.0,<25",  # Auto-formatter and linter
+        "mypy>=1.11.1,<2",  # Static type analyzer
         "types-requests",  # Needed for mypy type shed
         "types-setuptools",  # Needed for mypy type shed
-        "flake8>=7.0.0,<8",  # Style linter
+        "flake8>=7.1.1,<8",  # Style linter
         "isort>=5.13.2,<6",  # Import sorting linter
         "mdformat>=0.7.17,<0.8",  # Docs formatter and linter
         "mdformat-pyproject>=0.0.1",  # Allows configuring in pyproject.toml
@@ -58,7 +58,7 @@ setup(
     url="https://github.com/ApeWorX/ape-safe",
     include_package_data=True,
     install_requires=[
-        "eth-ape>=0.8.1,<0.9",
+        "eth-ape>=0.8.14,<0.9",
         "requests>=2.31.0,<3",
         "eip712",  # Use same version as eth-ape
         "click",  # Use same version as eth-ape
