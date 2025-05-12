69a2545fdc5437cca4e09bb2fd476f0843a04cf0
fubuloubu <3859395+fubuloubu@users.noreply.github.com>
2025-05-12 12:10:33 -0400

    test: refactor complex logic into `exec_transaction` fixture

--- tests/functional/test_account.py
+++ tests/functional/test_account.py
@@ -1,8 +1,6 @@
 import pytest
 from ape.api import AccountAPI
-from ape.exceptions import SignatureError
 from ape.types import AddressType
-from eth_utils import add_0x_prefix
 
 
 def test_init(safe, OWNERS, THRESHOLD, safe_contract):
@@ -12,11 +10,7 @@ def test_init(safe, OWNERS, THRESHOLD, safe_contract):
     assert safe.next_nonce == 0
 
 
-@pytest.mark.parametrize("mode", ("impersonate", "api", "sign"))
-def test_swap_owner(safe, accounts, OWNERS, mode):
-    impersonate = mode == "impersonate"
-    submit = mode != "api"
-
+def test_swap_owner(safe, accounts, OWNERS, exec_transaction):
     old_owner = safe.signers[0]
     new_owner = accounts[len(OWNERS)]  # replace owner 1 with account N + 1
     assert new_owner.address not in safe.signers
@@ -24,45 +18,12 @@ def test_swap_owner(safe, accounts, OWNERS, mode):
 
     prev_owner = safe.compute_prev_signer(old_owner)
 
-    def exec_transaction():
-        return safe.contract.swapOwner(
-            prev_owner,
-            old_owner,
-            new_owner,
-            sender=safe,
-            impersonate=impersonate,
-            submit=submit,
-        )
-
-    if submit:
-        receipt = exec_transaction()
-
-    else:
-        # Attempting to execute should raise `SignatureError` and push `safe_tx` to mock client
-        size = len(list(safe.client.get_transactions(confirmed=False)))
-        assert size == 0
-
-        with pytest.raises(SignatureError):
-            exec_transaction()
-
-        pending_txns = list(safe.client.get_transactions(confirmed=False))
-        assert len(pending_txns) == 1
-        assert len(pending_txns[0].confirmations) >= 1
-        safe_tx_hash = add_0x_prefix(f"{pending_txns[0].safe_tx_hash}")
-
-        safe_tx_data = pending_txns[0]
-        safe_tx = safe.create_safe_tx(**safe_tx_data.model_dump(by_alias=True, mode="json"))
-
-        # Ensure client confirmations works
-        client_confs = list(safe.client.get_confirmations(safe_tx_hash))
-        assert len(client_confs) >= 1
-
-        # Ensure API confirmations work
-        api_confs = safe.get_api_confirmations(safe_tx)
-        assert len(api_confs) >= 1
-
-        # `safe_tx` is in mock client, extract it and execute it successfully this time
-        receipt = safe.submit_safe_tx(safe_tx)
+    receipt = exec_transaction(
+        safe.contract.swapOwner,
+        prev_owner,
+        old_owner,
+        new_owner,
+    )
 
     assert receipt.events == [
         safe.contract.RemovedOwner(owner=old_owner),
@@ -74,38 +35,15 @@ def test_swap_owner(safe, accounts, OWNERS, mode):
     assert new_owner.address in safe.signers
 
 
-@pytest.mark.parametrize("mode", ("impersonate", "api", "sign"))
-def test_add_owner(safe, accounts, OWNERS, mode):
-    impersonate = mode == "impersonate"
-    submit = mode != "api"
-
+def test_add_owner(safe, accounts, OWNERS, exec_transaction):
     new_owner = accounts[len(OWNERS)]  # replace owner 1 with account N + 1
     assert new_owner.address not in safe.signers
 
-    def exec_transaction():
-        return safe.contract.addOwnerWithThreshold(
-            new_owner,
-            safe.confirmations_required,
-            sender=safe,
-            impersonate=impersonate,
-            submit=submit,
-        )
-
-    if submit:
-        receipt = exec_transaction()
-
-    else:
-        # Attempting to execute should emit a `SignatureError` and push `safe_tx` to mock client
-        assert len(list(safe.client.get_transactions(confirmed=False))) == 0
-        with pytest.raises(SignatureError):
-            exec_transaction()
-
-        assert len(list(safe.client.get_transactions(confirmed=False))) == 1
-
-        # `safe_tx` is in mock client, extract it and execute it successfully this time
-        safe_tx_data = next(safe.client.get_transactions(confirmed=False))
-        safe_tx = safe.create_safe_tx(**safe_tx_data.model_dump(by_alias=True, mode="json"))
-        receipt = safe.submit_safe_tx(safe_tx)
+    receipt = exec_transaction(
+        safe.contract.addOwnerWithThreshold,
+        new_owner,
+        safe.confirmations_required,
+    )
 
     assert receipt.events == [
         safe.contract.AddedOwner(owner=new_owner),
@@ -116,9 +54,7 @@ def test_add_owner(safe, accounts, OWNERS, mode):
 
 
 @pytest.mark.parametrize("mode", ["impersonate", "api", "sign"])
-def test_remove_owner(safe, OWNERS, mode):
-    impersonate = mode == "impersonate"
-    submit = mode != "api"
+def test_remove_owner(safe, OWNERS, exec_transaction):
     if len(OWNERS) == 1:
         pytest.skip("Can't remove the only owner")
 
@@ -128,32 +64,13 @@ def test_remove_owner(safe, OWNERS, mode):
 
     prev_owner = safe.compute_prev_signer(old_owner)
 
-    def exec_transaction():
-        return safe.contract.removeOwner(
-            prev_owner,
-            old_owner,
-            # Can't set the threshold to zero or more than the number of owners after removal
-            new_threshold,
-            sender=safe,
-            impersonate=impersonate,
-            submit=submit,
-        )
-
-    if submit:
-        receipt = exec_transaction()
-
-    else:
-        # Attempting to execute should emit a `SignatureError` and push `safe_tx` to mock client
-        assert len(list(safe.client.get_transactions(confirmed=False))) == 0
-        with pytest.raises(SignatureError):
-            exec_transaction()
-
-        assert len(list(safe.client.get_transactions(confirmed=False))) == 1
-
-        # `safe_tx` is in mock client, extract it and execute it successfully this time
-        safe_tx_data = next(safe.client.get_transactions(confirmed=False))
-        safe_tx = safe.create_safe_tx(**safe_tx_data.model_dump(by_alias=True, mode="json"))
-        receipt = safe.submit_safe_tx(safe_tx)
+    receipt = exec_transaction(
+        safe.contract.removeOwner,
+        prev_owner,
+        old_owner,
+        # Can't set the threshold to zero or more than the number of owners after removal
+        new_threshold,
+    )
 
     expected_events = [
         safe.contract.RemovedOwner(owner=old_owner),
