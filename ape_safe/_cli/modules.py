aac1338f151b676c0efb03d7e57abe535549bf6f
fubuloubu <3859395+fubuloubu@users.noreply.github.com>
2025-08-28 11:01:20 -0400

    refactor(CLI): remove typing from modules cli, causes issue w/ Click

--- ape_safe/_cli/modules.py
+++ ape_safe/_cli/modules.py
@@ -1,10 +1,8 @@
 import click
-from ape.api import AccountAPI  # noqa: TC002
-from ape.cli import ConnectedProviderCommand, account_option, network_option
+from ape.cli import ConnectedProviderCommand, account_option, ape_cli_context, network_option
 from ape.types import AddressType
 
 from ape_safe._cli.click_ext import safe_argument
-from ape_safe.accounts import SafeAccount  # noqa: TC001
 
 
 @click.group()
@@ -17,7 +15,7 @@ def modules():
 @modules.command("list", cls=ConnectedProviderCommand)
 @network_option()
 @safe_argument
-def _list(safe: SafeAccount):
+def _list(safe):
     """List all modules enabled for SAFE"""
     for module in safe.modules:
         click.echo(repr(module))
@@ -26,7 +24,7 @@ def _list(safe: SafeAccount):
 @modules.command(cls=ConnectedProviderCommand)
 @network_option()
 @safe_argument
-def guard(safe: SafeAccount):
+def guard(safe):
     """Show module guard (if enabled) for SAFE"""
     if guard := safe.modules.guard:
         click.echo(f"Guard: {guard}")
@@ -36,30 +34,34 @@ def guard(safe: SafeAccount):
 
 
 @modules.command(cls=ConnectedProviderCommand)
+@ape_cli_context()
 @network_option()
 @account_option()
 @safe_argument
 @click.option("--propose", is_flag=True, default=False)
-@click.argument("module", type=AddressType)
-def enable(safe: SafeAccount, account: AccountAPI, module: AddressType, propose: bool):
+@click.argument("module")
+def enable(cli_ctx, safe, account, module, propose):
     """
     Enable MODULE for SAFE
 
     **WARNING**: This is a potentially destructive action, and may make your safe vulnerable.
     """
+    module = cli_ctx.conversion_manager.convert(module, AddressType)
     safe.modules.enable(module, submitter=account, propose=propose)
 
 
 @modules.command(cls=ConnectedProviderCommand)
+@ape_cli_context()
 @network_option()
 @account_option()
 @safe_argument
 @click.option("--propose", is_flag=True, default=False)
-@click.argument("module", type=AddressType)
-def disable(safe: SafeAccount, account: AccountAPI, module: AddressType, propose: bool):
+@click.argument("module")
+def disable(cli_ctx, safe, account, module, propose):
     """
     Disable MODULE for SAFE
 
     **WARNING**: This is a potentially destructive action, and may impact operations of your safe.
     """
+    module = cli_ctx.conversion_manager.convert(module, AddressType)
     safe.modules.disable(module, submitter=account, propose=propose)
