fd98a235bb0d518e35dd9116b98186bc49229a24
fubuloubu <3859395+fubuloubu@users.noreply.github.com>
2025-10-06 14:44:33 -0400

    refactor(Utils): give back `HexBytes` instead of string

--- ape_safe/utils.py
+++ ape_safe/utils.py
@@ -1,22 +1,19 @@
 from collections.abc import Mapping
-from typing import TYPE_CHECKING, cast
+from typing import TYPE_CHECKING
 
+from ape.types import AddressType, HexBytes, MessageSignature
 from eip712.messages import calculate_hash
 from eth_utils import to_hex, to_int
 
 if TYPE_CHECKING:
-    from ape.types import AddressType, MessageSignature
-
-    from ape_safe.client.types import SafeTxID
+    from .client.types import SafeTx, SafeTxID
 
 
-def order_by_signer(
-    signatures: Mapping["AddressType", "MessageSignature"],
-) -> list["MessageSignature"]:
+def order_by_signer(signatures: Mapping[AddressType, MessageSignature]) -> list[MessageSignature]:
     # NOTE: Must order signatures in ascending order of signer address (converted to int)
     return [signatures[signer] for signer in sorted(signatures, key=lambda a: to_int(hexstr=a))]
 
 
-def get_safe_tx_hash(safe_tx) -> "SafeTxID":
+def get_safe_tx_hash(safe_tx: "SafeTx") -> "SafeTxID":
     message_hash = calculate_hash(safe_tx.signable_message)
-    return cast("SafeTxID", to_hex(message_hash))
+    return HexBytes(to_hex(message_hash))
