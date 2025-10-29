77ee3e3124acb020ca6b502eb574ee1eb0b85663
fubuloubu <3859395+fubuloubu@users.noreply.github.com>
2025-10-29 16:33:33 -0400

    feat(SDK): add `.cli` module w/ `propose_batch` decorator

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     install_requires=[
         "click>=8.1.7,<9",
         "eip712>=0.2.11,<3",
-        "eth-ape>=0.8.28,<0.9",
+        "eth-ape>=0.8.40,<0.9",
         "eth-utils>=2.1.0,<6",
         "pydantic>=2.10.2,<3",
         "requests>=2.31.0,<3",
