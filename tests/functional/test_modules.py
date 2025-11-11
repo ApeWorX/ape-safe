1f263eb3c8cac338b0cccf57ca75b912718c5cb3
fubuloubu <3859395+fubuloubu@users.noreply.github.com>
2025-11-11 17:46:55 -0500

    test: fix error string match since <1.3.0 Safe doesn't use GS codes

--- tests/functional/test_modules.py
+++ tests/functional/test_modules.py
@@ -1,5 +1,6 @@
 import pytest
 from ape.exceptions import ContractLogicError
+from packaging.version import Version
 
 
 @pytest.mark.parametrize("num_modules", [1, 2, 3])
@@ -36,10 +37,20 @@ def test_remove_module(safe, create_module, num_modules):
 
 def test_module_works(safe, create_module, deployer):
     module = create_module()
-    with pytest.raises(ContractLogicError, match="GS104"):
+
+    with pytest.raises(
+        # NOTE: `handle_safe_logic_error` won't work on a generic contract call
+        ContractLogicError,
+        match=(
+            "GS104"
+            # NOTE: Safe error codes only available in Safe v1.3.0+
+            if safe.version >= Version("1.3.0")
+            else "Method can only be called from an enabled module"
+        ),
+    ):
         module.test(safe, sender=deployer)
 
-    safe.modules.enable(module)
+    safe.modules.enable(module, submitter=deployer)
     assert module in safe.modules
 
     module.test(safe, sender=deployer)
